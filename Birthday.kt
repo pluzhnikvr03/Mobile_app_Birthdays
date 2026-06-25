4a130d6b1710aa5e587311dfac01bfa29e544d0b
Victoria <150126357+pluzhnikvr03@users.noreply.github.com>
2026-06-25 12:31:37 +0300

    Update Birthday.kt

--- Birthday.kt
+++ Birthday.kt
@@ -1,8 +1,10 @@
 package ru.hse.mobilepractice.birthdays
 
+// Карточка данных одного элемента.
 data class Birthday(
     val id: Int,
     val name: String,
     val date: String,
-    val isCongratulated: Boolean = false
+    val isCongratulated: Boolean = false,
+    val iconType: Int = 0 // 0 = WineBar, 1 = Cake, 2 = DateRange
 )
